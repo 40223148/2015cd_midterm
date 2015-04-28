f457f6dc8444b8a35202dc28e97b546e85d380d1
40223148 <40223148@gmail.com>
2015-04-28 12:06:04 +0800

    111

--- wsgi.py
+++ wsgi.py
@@ -108,6 +108,7 @@ class Hello(object):
     <form method=\"post\" action=\"dowork\">
         <fieldset>
         <legend>齒輪參數表單:</legend>
+        學號:40223148<br />
         齒數:<br />
         <input type=\"text\" name=\"N\"><br />
 
